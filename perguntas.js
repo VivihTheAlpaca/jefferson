7642b0d239b3986d928b9fc5d1882126d94d2c6e
Vih <vitor.wood@escola.pr.gov.br>
2025-05-31 02:11:12 +0000

    Jeffersonls

---
+++ perguntas.js
@@ -0,0 +1,73 @@
+criaCartao(
+    'Ordem Paranormal',
+    'O que é Ordem paranormal?',
+    'Ordem Paranormal é um sistema de RPG '
+)
+
+criaCartao(
+    'D&D',
+    'Qual a versão atual de D&D',
+    'A versão mais atual de D&D'
+)
+
+criaCartao(
+    'Chamado de Cthulhu',
+    'Como funciona o sistema de sanidade?',
+    'Os personagens perdem sanidade ao enfrentar horrores cósmicos, o que pode levar à loucura.'
+)
+
+criaCartao(
+    'Chamado de Cthulhu',
+    'Quem criou o Chamado de Cthulhu?',
+    'O sistema é inspirado nas obras de H.P. Lovecraft e foi criado pela Chaosium em 1981.'
+
+)
+
+criaCartao(
+    'Ordem Paranormal',
+    'Qual o foco do RPG Ordem Paranormal?',
+    'Mistura terror investigativo com ação, onde agentes enfrentam o ocultismo e criaturas do Outro Lado.'
+)
+
+criaCartao(
+    'Ordem Paranormal',
+    'O que é o Outro Lado?',
+    'É uma dimensão paralela cheia de entidades e mistérios, ligada ao ocultismo.'
+)
+
+criaCartao(
+    'Tormenta20',
+    'O que é a Tormenta em T20?',
+    'É uma tempestade mágica caótica e mortal, que destrói tudo e transforma criaturas.'
+)
+
+criaCartao(
+    'Tormenta20',
+    'Qual dado principal em T20?',
+    'O dado de 20 lados (d20) é o principal usado em quase todas as jogadas.'
+)
+
+criaCartao(
+     'D&D',
+    'O que significa D&D?',
+    'Significa Dungeons & Dragons, o RPG mais famoso do mundo.'
+)
+
+criaCartao(
+    'Ordem Paranormal',
+    'Quem criou Ordem Paranormal?',
+    'Ordem Paranormal foi criado por Cellbit, originalmente como uma websérie e depois adaptado para RPG.'
+)
+
+criaCartao(
+    'Tormenta20',
+    'O que é um lefou?',
+    'Lefou são pessoas marcadas pela Tormenta — mutantes com poderes e deformações.'
+)
+
+criaCartao(
+    'Chamado de Cthulhu',
+    'Qual o maior perigo no Chamado de Cthulhu?',
+    'O medo, a insanidade e o conhecimento proibido — mais que monstros, o horror vem da mente.'
+)
+
